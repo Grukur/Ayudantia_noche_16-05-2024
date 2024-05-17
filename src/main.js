7429a98b16da5fe3a5dd002b6226292d39c18153
Grukur <darold.trench@gmail.com>
2024-05-16 22:51:10 -0400

    start

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Cliente from "./Cliente.js";
+import { Impuestos } from "./Impuestos.js";
+
+let impuestoCliente = new Impuestos(10000, 2000);
+let clientePrueba = new Cliente('Diego', impuestoCliente)
+
+console.log(impuestoCliente.montoBrutoAnual, impuestoCliente.deducciones)
+console.log(clientePrueba.nombre, clientePrueba.impuesto.montoBrutoAnual, clientePrueba.impuesto.deducciones)
+console.log(clientePrueba.calcularImpuesto())
+
+
+
+/* let persona = new Cliente('Alvaro', 300);
+console.log(persona.nombre, persona.impuesto); */
